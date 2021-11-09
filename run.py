862d9bac4d1c874cd3e085f7ed985fb316ebd682
rodrigodadam <rodrigodadam@gmail.com>
2021-11-09 11:06:51 +0000

    Insert data to company.json

--- run.py
+++ run.py
@@ -13,7 +13,10 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html", page_title="About", list_of_numbers=[1, 2, 3])
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company=data)
 
 
 @app.route("/contact")
